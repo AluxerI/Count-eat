cdbe6264bace5e1ff329c53ce8cbf132e0274948
Carame11er <161019858+Carame11er@users.noreply.github.com>
2024-04-17 23:15:44 +0300

    added hidden and visible blocks

--- count-eat/script.js
+++ count-eat/script.js
@@ -1,92 +1,108 @@
-(function () {
-  "use strict";
+"use strict";
 
-  var carousel = document.getElementsByClassName("carousel")[0],
-    slider = carousel.getElementsByClassName("carousel__slider")[0],
-    items = carousel.getElementsByClassName("carousel__slider__item"),
-    prevBtn = carousel.getElementsByClassName("carousel__prev")[0],
-    nextBtn = carousel.getElementsByClassName("carousel__next")[0];
+var carousel = document.getElementsByClassName("carousel")[0],
+  slider = carousel.getElementsByClassName("carousel__slider")[0],
+  items = carousel.getElementsByClassName("carousel__slider__item"),
+  prevBtn = carousel.getElementsByClassName("carousel__prev")[0],
+  nextBtn = carousel.getElementsByClassName("carousel__next")[0];
 
-  var width,
-    height,
-    totalWidth,
-    margin = 20,
-    currIndex = 0,
-    interval,
-    intervalTime = 4000;
+var width,
+  height,
+  totalWidth,
+  margin = 20,
+  currIndex = 0,
+  interval,
+  intervalTime = 4000;
 
-  function init() {
-    resize();
-    move(Math.floor(items.length / 2));
-    bindEvents();
+function resize() {
+  (width = Math.max(window.innerWidth * 0.25, 275)),
+    (height = window.innerHeight * 0.5),
+    (totalWidth = width * items.length);
 
-    timer();
+  slider.style.width = totalWidth + "px";
+
+  for (var i = 0; i < items.length; i++) {
+    let item = items[i];
+    item.style.width = width - margin * 2 + "px";
+    item.style.height = height + "px";
   }
+}
 
-  function resize() {
-    (width = Math.max(window.innerWidth * 0.25, 275)),
-      (height = window.innerHeight * 0.5),
-      (totalWidth = width * items.length);
+function move(index) {
+  if (index < 1) index = items.length;
+  if (index > items.length) index = 1;
+  currIndex = index;
 
-    slider.style.width = totalWidth + "px";
-
-    for (var i = 0; i < items.length; i++) {
-      let item = items[i];
-      item.style.width = width - margin * 2 + "px";
-      item.style.height = height + "px";
+  for (var i = 0; i < items.length; i++) {
+    let item = items[i],
+      box = item.getElementsByClassName("item__3d-frame")[0];
+    if (i == index - 1) {
+      item.classList.add("carousel__slider__item--active");
+      box.style.transform = "perspective(1200px)";
+    } else {
+      item.classList.remove("carousel__slider__item--active");
+      box.style.transform =
+        "perspective(1200px) rotateY(" + (i < index - 1 ? 40 : -40) + "deg)";
     }
   }
 
-  function move(index) {
-    if (index < 1) index = items.length;
-    if (index > items.length) index = 1;
-    currIndex = index;
+  slider.style.transform =
+    "translate3d(" +
+    (index * -width + width / 2 + window.innerWidth / 2) +
+    "px, 0, 0)";
+}
 
-    for (var i = 0; i < items.length; i++) {
-      let item = items[i],
-        box = item.getElementsByClassName("item__3d-frame")[0];
-      if (i == index - 1) {
-        item.classList.add("carousel__slider__item--active");
-        box.style.transform = "perspective(1200px)";
-      } else {
-        item.classList.remove("carousel__slider__item--active");
-        box.style.transform =
-          "perspective(1200px) rotateY(" + (i < index - 1 ? 40 : -40) + "deg)";
-      }
-    }
-
-    slider.style.transform =
-      "translate3d(" +
-      (index * -width + width / 2 + window.innerWidth / 2) +
-      "px, 0, 0)";
-  }
-
-  function timer() {
-    clearInterval(interval);
-    interval = setInterval(() => {
-      move(++currIndex);
-    }, intervalTime);
-  }
-
-  function prev() {
-    move(--currIndex);
-    timer();
-  }
-
-  function next() {
+function timer() {
+  clearInterval(interval);
+  interval = setInterval(() => {
     move(++currIndex);
-    timer();
-  }
+  }, intervalTime);
+}
 
-  function bindEvents() {
-    window.onresize = resize;
-    prevBtn.addEventListener("click", () => {
-      prev();
-    });
-    nextBtn.addEventListener("click", () => {
-      next();
-    });
-  }
+function prev() {
+  move(--currIndex);
+  timer();
+}
 
-  init();
-})();
+function next() {
+  move(++currIndex);
+  timer();
+}
+
+function bindEvents() {
+  window.onresize = resize;
+  prevBtn.addEventListener("click", () => {
+    prev();
+  });
+  nextBtn.addEventListener("click", () => {
+    next();
+  });
+}
+
+resize();
+move(Math.floor(items.length / 2));
+bindEvents();
+
+timer();
+
+const foodBtns = document.querySelectorAll(".wrapper-item");
+const recipeElement = document.querySelector(".recipe");
+const foodItemElement = document.querySelector(".food-item");
+const backBtn = document.querySelector(".back_button");
+
+foodBtns.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    e.preventDefault();
+    console.log(button);
+    foodItemElement.classList.add("hidden");
+    recipeElement.classList.remove("hidden");
+    backBtn.classList.remove("hidden");
+  });
+});
+
+backBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  foodItemElement.classList.remove("hidden");
+  recipeElement.classList.add("hidden");
+  backBtn.classList.add("hidden");
+});
